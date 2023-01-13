22a67194ff48df90e7183b535ea92b6218d97b34
Jim <jimkernicky@gmail.com>
2023-01-12 19:49:26 -0500

    Major rework

--- README.md
+++ README.md
@@ -1,18 +1,55 @@
 # Pre and Code tag formatter
 
-Output code symbols/syntax for display in pre and code tags.
+Output code symbols/syntax for display in a pre tag codeblock.
 
-> Right now the page is sloppy and I need to have 2 things: 1) have each line of code break to a new line for each of copying, 2) how to grab the code from a file or from a textarea?
+Things I need:
 
-I Need: 1) a form with a textarea element, 2) a select list for the language you want to use for styling, 3) and finally a submit button.
+1. convert html entities (**DONE**)
+1. wrap each line in a span tag with a class for the default color (_DONE_)
+1. use RegEx to add span with classes for various parts of the code
 
-## CSS classes
+Nice to have:
 
-- `.container`
-- `.code` or `.noncode` on `pre` tag for the background color for the code block which contains all the span tags. `.code` outputs a dark blueish color, `.noncode` outputs very light gray background
-- `css` on `span` tag for CSS, outputs green text
-- `html` on `span` tag for HTML, outputs yellow text
-- `php` on `span` tag for PHP, outputs white text
-- `javascript` on `span` tag for JavaScript, outputs orange
-- `css` on `span` tag for CSS
-- `span.code` for "non-code", outputs black text
+1. have each line of code break to a new line for easier copy/paste,
+1. how to grab the code from a code file or...
+1. from a form with a textarea element,
+1. a select list for the language you want to use for styling,
+1. and a submit button that runs the JS code and outputs the code back onto the page (async/await)
+
+## Colors and classes
+
+> Note: I am using a dark theme in VS Code
+
+Right now in my editor I see 8 colors: 2 blues, 1 gray, 1 white, 1 lime green, 1 orange, 1 red, and 1 purple. That is from html, css, js, md, and php.
+
+1. a common light blue: HTML attr values, quoted values in CSS and JS,
+1. a darker light blue: JS var names, CSS values, HTML attributes
+1. gray is for comments
+1. white is for HTML content and angle brackets, JS let var names and punctuation (. , ;, :), CSS punctuation (, ;, :)
+1. red is for CSS units, JS keywords & symbols
+1. purple is for JS function/method names
+1. orange: markdown list symbols | JS parameters and brackets depending on scope
+1. lime green: css tags, prop names and parens; JS brackets depending on scope; html element names | markdown comments
+
+CSS: only green, light blue, dark blue, red, white
+
+HTML: only green, white, light blue, dark blue
+
+JS: make `[]` = blue, `{}` = orange, `()` = orange | `.,;` = white
+
+PHP:
+
+Markdown:
+
+## Color table
+
+def = Default
+
+| Color | Lang | uses               |
+| :---- | :--- | :----------------- |
+| White | MD   | def                |
+|       | CSS  | `:`, `,`, `;`      |
+|       | HTML | content            |
+|       |      | `<`, `>`, `/`, `=` |
+
+Make the precode default white so classes do not need to be put on puctuation
--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-const code = document.getElementById("code");
-const nonCode = document.getElementById("noncode");
+const darkBlockOutput = document.getElementById("dark_block_output");
+const lightBlockOutput = document.getElementById("light_block_output");
 
-const classes = ["css", "html", "javascript", "php"];
-const codeClass = classes[2];
+const languages = ["css", "html", "javascript", "php", "markdown"];
+const classes = ["white", "light-blue", "dark-blue", "lime-green", "comment", "red", "purple", "orange"]
+
+// all languages seem to use light-blue for values in single or double-quotes so I can rule that color out as the default color and just use RegEx to add a span.light-blue around the quotes and the contents
+
+const codeToConvert = [
+  `<p class="test">words here</p>`,
+  `<title>Code Formatter</title>`,
+  `<img src ="./img/file.jpeg" />`
+]
+
+const myRegex = /&quot;[.\w\/:*?-]*/g;
+const myRegex2 = /(&quot;[.\w\/:*?-]*\w)(&quot;[.\w\/:*?-]*)/g;
+
+let codeClass = "";
+const lang = "html";
+
+// Set default color class by language
+switch (lang) {
+  case "html": 
+  case "css": 
+    codeClass = classes[0];
+    break
+  case "javascript": 
+  case "php": 
+  case "markdown": 
+    codeClass = classes[2];
+    break
+}
 
 function convertHTML(str) {
   const convertSymbols = {
@@ -19,17 +46,44 @@ function convertHTML(str) {
     .join("");
 }
 
-const output = ["const users = ", '   { name: "John_&_Jane", age: 34 },', '  { name: "Amy_<>_1", age: 20 },', ' { name: "SINGLE-QUOTE_\'_1", age: 10 }', "];"];
+// For now I'm adding lines to an array - need to find something better
+const input = [`<p class="test">words here</p>`, `<title>Code Formatter</title>`, `<img src ="./img/file.jpeg" />`];
 
-output.forEach(line => {
+let testArr = [];
+
+input.forEach(line => {
   let convertedLine = convertHTML(line);
-  code.textContent += '<span class="' + `${codeClass}` + '">' + `${convertedLine}` + "</span>" + `\r`;
+
+  testArr.push(`${convertedLine}`);
+});
+console.log(testArr)
+
+let result = '';
+let matches = testArr.map(item => {
+  let newArr = []
+  if (item.match(myRegex)) {
+    let cleanHtml = item.match(myRegex).toString().split(",").join("")
+    newArr.push(`<span class="light-blue">${cleanHtml}</span>`) 
+  } else {
+    newArr.push(item)
+  }
+  return newArr
+})
+
+
+matches.forEach(line => {
+  if (line.match(myRegex)) {
+    result = line.replace(myRegex, `<span class="light-blue">${line.match(myRegex)}</span>`);
+  } else {
+    result = line;
+  }
+
+  darkBlockOutput.textContent += '<span class="' + `${codeClass}` + '">' + `${result}` + "</span>";
 });
 
-output.forEach(line => {
-  let testOutput = [];
+
+// Ignore below here - just the light code block
+input.forEach(line => {
   let convertedLine = convertHTML(line);
-  // testOutput.push()
-  // I need line breaks but can't get them. How do I get the code to output into div tags and innerHTML but still get the textContent? \r, <br />, and &nbsp; all don't work
-  nonCode.textContent += '<span class="code">' + `${convertedLine}` + "</span>" + `\r`;
+  lightBlockOutput.textContent += '<span class="' + `${codeClass}` + '">' + `${convertedLine}` + "</span>";
 });
